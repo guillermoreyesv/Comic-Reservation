1b96e3e125a40f13234726fa3757ba15bc9ed353
Guillermo <guillermo_hard@hotmail.com>
2023-02-13 13:42:09 -0600

    feat:addgetLayawayList

--- application/utils/user.py
+++ application/utils/user.py
@@ -1,3 +1,45 @@
 class User():
-    def getProfile():
-        return 'hola'
+    def validate_token(token):
+        from application import app
+        import requests
+        import os
+
+        response = None
+
+        if not token:
+            app.logger.error(f'token is {token}')
+            raise Exception('Not found token')
+            return response
+
+        start_with_bearer = token.startswith('Bearer ')
+
+        if not start_with_bearer:
+            app.logger.error(f'token not start with Bearer {token}')
+            raise Exception('Not found token')
+            return response
+
+        token = token[7:]
+
+        # Search token profile
+        try:
+            url = os.getenv('USER_MANAGMENT_URL')
+
+            headers = {
+                'Authorization': f'Bearer {token}'
+            }
+
+            response = requests.request("GET", url, headers=headers)
+            if response.status_code != 200:
+                raise Exception('invalid token')
+            response = response.json()
+
+        except Exception as e:
+            app.logger.error(f'Cant reach MS {e}')
+            raise Exception('Server error')
+            return response
+
+        if not response:
+            app.logger.error(f'User not found {token}')
+            raise Exception('Not found token')
+
+        return response
--- application/views/users.py
+++ application/views/users.py
@@ -5,4 +5,46 @@ from flask import request
 class LayawayList(MethodView):
     def get(self):
         from application.utils.user import User
-        return User.getProfile()
+        from application import app
+        response = {'code': 500, 'message': 'server failed'}
+        list_filters = ['onsaledate', 'title', 'id']
+
+        # Get Bearer Token
+        authorization = request.headers.get('Authorization')
+
+        # Validate Token
+        try:
+            info_user = User.validate_token(authorization)
+        except Exception as e:
+            response = {
+                'code': 400,
+                'message': 'Invalid token'
+            }
+            app.logger.error(f'Invalid token {e}')
+            return response, response['code']
+
+        # Get params
+        filter = request.args.get(key='orderBy', default='')
+
+        # Evaluate params
+        if filter.lower().strip() not in list_filters and filter:
+            response = {
+                'code': 400,
+                'message': 'Invalid Filter'
+            }
+            app.logger.error(f'Invalid Filter {filter}')
+            return response, response['code']
+
+        # Search comics
+        comic_list = info_user.get('comics_layaway')
+        if not comic_list:
+            response = {'code': 404, 'message': 'Not found comics layawayed'}
+            return response, response['code']
+
+        # Order comics
+        if filter:
+            comic_list = sorted(comic_list, key=lambda x: x[filter])
+
+        response = {'code': 200, 'message': 'Found comics layawayed'}
+        response['comics'] = comic_list
+        return response, response['code']
